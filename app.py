8d9d8260ab17bd4cc66fb7cf0ce90a5d8805aca2
Bio-hacked <Bio-hacked@users.noreply.github.com>
2021-02-02 01:58:01 -0500

    Beta testing RNA to Amino acid sequence == It works

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def RNAtranslation():
    
 
     RNA_sequence = ''
-    Protein_code = {
+    
+    Protein_code = { 
 
    "F": ["UUU", "UUC"],
    "L":["UUA", "UUG", "CUU", "CUC", "CUA", "CUG"],
@@ -39,26 +40,28 @@ def RNAtranslation():
   }
   
    #now that the codon table is created, create a loop that is able to iterate through the RNA sequence as a strings
-    Protein_string = ""
+    Amino_string = ""
 
-
+    if request.method == 'POST' and 'RNA_sequence' in request.form:
+        RNA_sequence = request.form.get('RNA_sequence')
+        
    #Begin by making a for loop that skips to the 3rd character after current index
-    for iteration in range(0, len(RNA_sequence), 3):
+        for iteration in range(0, len(RNA_sequence), 3):
     # Now build a piece of code that checks if the current index plus the next 2 characters are in the library 
 
-        for iteration_2 in Protein_code:
+            for iteration_2 in Protein_code:
       
 
       # Now I want to iterate through the lists contained in each dictionary keyword
-            for iteration_3 in Protein_code[iteration_2]:
+                for iteration_3 in Protein_code[iteration_2]:
 
-                if RNA_sequence[iteration: iteration + 3] == iteration_3:
-                    Protein_string += iteration_2
-                elif RNA_sequence[iteration: iteration + 3] in Protein_code["Stop"]:
-                    " Stop codon reached"
-                    break
+                    if RNA_sequence[iteration: iteration + 3] == iteration_3:
+                        Amino_string += iteration_2
+                    elif RNA_sequence[iteration: iteration + 3] in Protein_code["Stop"]:
+                        " Stop codon reached"
+                        break
     
-    return render_template('index.html')
+    return render_template('index.html',Amino_string=Amino_string, RNA_sequence=RNA_sequence)
 
 
 app.run()
